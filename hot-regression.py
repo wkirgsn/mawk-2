ca4860c78bf7b3df13f3f29be2525dd6fa65cefe
wkirgsn <wilkir@gmx.de>
2017-12-18 14:19:05 +0100

    finish train kerash, batchsize unknown yet

--- hot-regression.py
+++ hot-regression.py
@@ -6,6 +6,7 @@ import multiprocessing
 warnings.filterwarnings("ignore")
 np.random.seed(2017)
 
+DEBUG = False
 Input_param_names = ['ambient',
                      'coolant',
                      'u_d',
@@ -21,16 +22,16 @@ Target_param_names = ['pm',
                       'stator_winding']
 
 profile_id_colname = 'profile_id'
+lookback = 2
 
 valset = ['31', ]
 testset = ['20', ]
 loadsets = ['4', '6', '10', '11', '20', '27', '29', '30',
             '31', '32', '36']
 file_path = "/home/wilhelmk/Messdaten/PMSM_Lastprofile/hdf/all_load_profiles"
-subsequence_len = 100
 
 
-def munge_data(all_df, lookback=5, dropna=True):
+def munge_data(all_df, dropna=True):
     """load data for linear regressors"""
     def add_hist_data(df):
         """add previous x data points"""
@@ -63,6 +64,15 @@ def munge_data(all_df, lookback=5, dropna=True):
     test_df = all_df[all_df[profile_id_colname].isin(testset)]
     val_df = all_df[all_df[profile_id_colname].isin(valset)]
 
+    if DEBUG:
+        train_df = train_df.iloc[:100, :]
+        val_df = val_df.iloc[:100, :]
+        test_df = test_df.iloc[:100, :]
+
+    train_df.reset_index(drop=True, inplace=True)
+    test_df.reset_index(drop=True, inplace=True)
+    val_df.reset_index(drop=True, inplace=True)
+
     return train_df, val_df, test_df
 
 
@@ -74,117 +84,53 @@ def get_norm_stats(all):
 
 
 def train_keras():
-    def load_data_keras(hdf_file):
-        train = [pd.read_hdf(hdf_file, key='p' + str(k)) for k in loadsets if k
-                 not in valset or k not in testset]
-        val = [pd.read_hdf(hdf_file, key='p' + str(k)) for k in valset]
-        test = [pd.read_hdf(hdf_file, key='p' + str(k)) for k in testset]
-        return train, val, test
+    from keras.models import Sequential
+    from keras.layers import LSTM, GRU
+    from keras.layers.core import Dense, Dropout, Flatten
+    from keras.optimizers import SGD
+    from keras.callbacks import EarlyStopping
+    from keras import __version__ as keras_version
+
     print('Keras version: {}'.format(keras_version))
+    batch_size = 64
+    n_neurons = 8
+    n_epochs = 500
+    observation_len = 10
 
-    train_set, val_set, test_set = load_data_keras(file_path)
-    all_data = train_set + val_set + test_set
+    def create_dataset(dataset, observe=1):
+        dataX, dataY = [], []
+        for i in range(len(dataset) - observe):
+            dataX.append(dataset.loc[i:(i + observe), x_cols])
+            dataY.append(dataset.loc[i + observe, y_cols])
+        return np.array([p.as_matrix() for p in dataX]), \
+               np.array([p.as_matrix() for p in dataY])
 
-    # normalize data
-    s_mean, s_max, s_min = get_norm_stats(all_data)
-    all_data_normed = [(a - s_mean) / (s_max - s_min) for a in all_data]
+    X_tr, Y_tr = create_dataset(tra_df, observation_len-1)
+    X_val, Y_val = create_dataset(val_df, observation_len-1)
+    X_tst, Y_tst = create_dataset(tst_df, observation_len-1)
 
-    # print(all_data_normed[0].head(10))
-
-    test_set = all_data_normed.pop()
-    val_set = all_data_normed.pop()
-
-    val_in = val_set[Input_param_names].values
-    val_out = val_set[Target_param_names].values
-
-    test_in = test_set[Input_param_names].values
-    test_out = test_set[Target_param_names].values
-
-    input_dim = test_in.shape[1]
+    # reshape for keras (not necessary here)
+    # X_tr = np.reshape(X_tr, (X_tr.shape[0], 1, X_tr.shape[1]))
+    # X_tst = np.reshape(X_tst, (X_tst.shape[0], 1, X_tst.shape[1]))
+    print('n samples = {}'.format(X_tr.shape[0]))
 
     # create model
     model = Sequential()
-    model.add(GRU(32, batch_input_shape=(1, 1, input_dim),
-                  consume_less='cpu', stateful=True))
+    model.add(LSTM(n_neurons,
+                   batch_input_shape=(batch_size,
+                                      X_tr.shape[1],
+                                      X_tr.shape[2]), stateful=True))
     model.add(Dense(4))
 
-    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
-
-    callbacks = [
+    """callbacks = [
         EarlyStopping(monitor='val_loss', patience=3, verbose=0),
-    ]
+    ]"""
 
-    model.compile(optimizer=sgd, loss='mse')
-
-    """print('in shape: {} out shape: {}'.format(model_in.shape, model_out.shape))
-    model.fit(model_in, model_out, batch_size=1, nb_epoch=100,
-              validation_data=(val_in, val_out),
-              callbacks=callbacks)
-
-    score = model.evaluate(test_in, test_out)
-    print(score)"""
-
-    print('Train...')
-    for epoch in range(1, 15):
-        print('Epoch {}'.format(epoch))
-        mean_tr_loss = []
-        sample_count = 0
-        for sample in all_data_normed:
-            sample_count += 1
-            print('sample {} of {}'.format(sample_count, len(all_data_normed)))
-            # split input and output
-            model_in = sample[Input_param_names].values
-            model_out = sample[Target_param_names].values
-
-            for i in range(len(sample.index)):
-                tr_loss = \
-                    model.train_on_batch(
-                        np.expand_dims(
-                            np.expand_dims(model_in[i, :],
-                                           axis=0),
-                            axis=0),
-                        np.atleast_2d(model_out[i, :]))
-                mean_tr_loss.append(tr_loss)
-            model.reset_states()
-
-            print('loss training = {}'.format(np.mean(mean_tr_loss)))
-
-        mean_te_loss = []
-        for i in range(val_in.shape[0]):
-            te_loss = model.test_on_batch(
-                np.expand_dims(np.expand_dims(val_in[i, :], axis=0), axis=0),
-                np.atleast_2d(val_out[i, :]))
-            mean_te_loss.append(te_loss)
-        model.reset_states()
-
-        print('loss validation = {}'.format(np.mean(mean_te_loss)))
-        print('___________________________________')
-
-    print("Test..")
-
-    y_pred = []
-    for j in range(test_in.shape[0]):
-        batch = test_in[j, :]
-        y_pred.append(model.predict_on_batch(batch[np.newaxis, np.newaxis, :]))
-    y_pred = np.vstack(y_pred)
-    model.reset_states()
-
-    mean_te_loss = []
-    for i in range(test_in.shape[0]):
-        batch = test_in[i, :]
-        batch_y = test_out[i, :]
-        te_loss = model.test_on_batch(batch[np.newaxis, np.newaxis, :],
-                                      batch_y[np.newaxis, :])
-        mean_te_loss.append(te_loss)
-    model.reset_states()
-
-    print('loss test = {}'.format(np.mean(mean_te_loss)))
-    print('___________________________________')
-
-    time = np.arange(len(test_in.index), dtype=np.float32)
-    time /= (2 * 60)
-    plt.plot(time, test_out[:, 0])
-    plt.plot(time, y_pred[:, 0])
+    model.compile(optimizer='adam', loss='mse')
+    history = model.fit(X_tr, Y_tr, epochs=n_epochs, batch_size=batch_size,
+                        validation_data=(X_val, Y_val), verbose=1,
+                        shuffle=False)
+    return model.predict(X_tst)
 
 
 def tpotting():
@@ -242,19 +188,14 @@ if __name__ == '__main__':
     from sklearn.preprocessing import MinMaxScaler
 
     from sklearn.model_selection import train_test_split
-    from keras.models import Sequential
-    from keras.layers import LSTM, GRU
-    from keras.layers.core import Dense, Dropout, Flatten
-    from keras.optimizers import SGD
-    from keras.callbacks import EarlyStopping
-    from keras import __version__ as keras_version
 
     scaler = MinMaxScaler()
     dataset = pd.read_csv(join('input', 'measures.csv'), dtype=np.float32)
+
     orig_cols = [c for c in dataset.columns if c != profile_id_colname]
 
-    tra_df, val_df, tst_df = munge_data(dataset.copy(), lookback=2)
-    tra_df = pd.concat([tra_df, val_df], axis=0)
+    tra_df, val_df, tst_df = munge_data(dataset.copy())
+    # tra_df = pd.concat([tra_df, val_df], axis=0)
     x_cols = [col for input in Input_param_names for col in tra_df.columns if
               col[:3] in input]
     y_cols = Target_param_names
@@ -263,11 +204,13 @@ if __name__ == '__main__':
                            columns=orig_cols)
 
     # tpot
-    pred_df[y_cols] = tpotting()
+    #pred_df[y_cols] = tpotting()
 
     # linear model
     #pred_df[y_cols] = train_linear(tra_df, tst_df, x_cols, y_cols)
 
+    # keras
+    pred_df[y_cols] = train_keras()
 
     actual = \
         dataset[dataset[profile_id_colname].isin(testset)].loc[:, y_cols]
@@ -282,5 +225,5 @@ if __name__ == '__main__':
                                                  inversed_pred)))
 
     # plots
-    #plot_results(actual, inversed_pred)
+    plot_results(actual, inversed_pred)
 
